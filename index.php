6fb789e37e4c1627c2914456af75d20618920519
Evelin Landeros <levelinedith@gmail.com>
2017-10-11 23:48:28 -0500

    Segundo commit

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
 <?php
 require_once __DIR__.'/vendor/autoload.php';
 
+use GuzzleHttp\Client;
+use Symfony\Component\HttpFoundation\Response;
+
+$api_key = getenv ('OPEN_WEATHER_API');
+
 $app = new Silex\Application();
 
-$app->get('/clima', function() use($app) {
+$app->get('/clima', function() use($app,$api_key) {
+                
+        $client = new client();
+        $url = "http://api.openweathermap.org/data/2.5/weather?id=3530597&appid=ef42538a4eeebe4d2dc75034c3a0b20e&units=metric";
+        
+        $response = $client -> get ($url);
+        $body = $response -> getBody ();
 
-	//
-	
-	$array = ["hola" => "mundo"];
-
-    return $app->json(arreglo);
+        return new Response ($body,200,array('Content-Type' => 'application/json'));
 });
 
 $app->run();
+?>
